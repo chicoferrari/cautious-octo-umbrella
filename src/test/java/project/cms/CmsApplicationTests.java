5db8ea36b941c679d55c120a3262667a42909129
Rafael F <rafa.ferrari@outlook.com>
2021-02-28 07:28:39 -0300

    Commit inicial.

---
+++ src/test/java/project/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
